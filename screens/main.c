05acae9d57256a753c73e592b5e0673ea2c3266d
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-01-26 15:00:25 +0530

    Implemented Character Sprite Mapping & Animation

--- screens/main.c
+++ screens/main.c
@@ -3,7 +3,7 @@
 #include <raymath.h>
 #include <stdio.h>
 
-GameScreen currentScreen = TITLE;
+GameScreen currentScreen = GAMEPLAY;
 
 void UpdateDrawFrame(void);
 Vector2 GetCenterTileLocation();
@@ -11,7 +11,7 @@ Vector2 GetCenterTileLocation();
 int main() {
   InitWindow(1280, 720, "Test Game");
 
-  InitTitleScreen();
+  // InitTitleScreen();
   InitGameScreen();
 
   SetTargetFPS(60);
@@ -24,7 +24,7 @@ int main() {
     UnloadGameScreen();
     break;
   case TITLE:
-    UnloadTitleScreen();
+    // UnloadTitleScreen();
     break;
   default:
     break;
@@ -41,7 +41,7 @@ void UpdateDrawFrame(void) {
     UpdateGameScreen();
     break;
   case TITLE:
-    UpdateTitleScreen();
+    // UpdateTitleScreen();
     break;
   default:
     break;
@@ -54,7 +54,7 @@ void UpdateDrawFrame(void) {
     DrawGameScreen();
     break;
   case TITLE:
-    DrawTitleScreen();
+    // DrawTitleScreen();
     break;
   default:
     break;
--- screens/screen_game.c
+++ screens/screen_game.c
@@ -7,30 +7,53 @@ static Texture2D background = {0};
 static Texture2D player = {0};
 static Camera2D camera = {0};
 
+Rectangle frameRec = {0};
+int currentFrame = 0;
+int framesCounter = 0;
+int framesSpeed = 0;
+
 static int width;
 static int height;
 
 static int finishScreen = 0;
+
+bool face_right = false;
+bool face_left = false;
+bool face_top = false;
+bool face_bot = false;
 
 void InitGameScreen(void)
 {
   // Initialize player position
   playerPosition.x = 0.0f;
   playerPosition.y = 0.0f;
+
   // Initialize the center tile location. This is used for mapping the 9
   // background tiles.
   centerTile.x = 0;
   centerTile.y = 0;
+
   // Loading Textures
   background = LoadTexture("resources/background.png");
-  player = LoadTexture("resources/book.png");
+
+  Image image = LoadImage("resources/char.png");
+  ImageResizeNN(&image, image.width * 4, image.height * 4);
+  player = LoadTextureFromImage(image);
+
   // Getting width and height
   width = GetScreenWidth();
   height = GetScreenHeight();
+
+  frameRec = (Rectangle){(float)player.width / 10, 17.0 * (float)player.height / 20, (float)player.width / 10, (float)player.height / 20};
+
+  currentFrame = 0;
+  framesCounter = 0;
+  framesSpeed = 8;
+
   // Initializing the camera
   camera.offset = (Vector2){0, 0};
-  camera.target = (Vector2){.x = playerPosition.x - width / 2,
-                            .y = playerPosition.y - GetScreenHeight() / 2};
+  camera.target = (Vector2){.x = playerPosition.x - width / 2 + player.width / 20,
+                            .y = playerPosition.y - GetScreenHeight() / 2 + player.height / 40};
   camera.rotation = 0;
   camera.zoom = 1.0f;
 };
@@ -39,29 +62,62 @@ Vector2 GetCenterTileLocation();
 
 void UpdateGameScreen(void)
 {
-  // Player movement
+  framesCounter++;
+
+  if (framesCounter >= (60 / framesSpeed))
+  {
+    framesCounter = 0;
+    currentFrame++;
+
+    if (currentFrame > 1)
+      currentFrame = 0;
+
+    if (face_left == true || face_right == true)
+      frameRec.x = (float)currentFrame * (float)player.width / 10 + (float)player.width / 10 * 8;
+    if (face_top == true)
+      frameRec.x = (float)currentFrame * (float)player.width / 10 + (float)player.width / 10 * 6;
+    if (face_bot == true)
+      frameRec.x = (float)currentFrame * (float)player.width / 10 + (float)player.width / 10 * 4;
+  }
+
   if (IsKeyDown(KEY_UP))
   {
     playerPosition.y -= 10;
+    face_top = true;
+    face_bot = false;
+    face_right = false;
+    face_left = false;
   }
-  if (IsKeyDown(KEY_DOWN))
+  else if (IsKeyDown(KEY_DOWN))
   {
     playerPosition.y += 10;
+    face_bot = true;
+    face_top = false;
+    face_right = false;
+    face_left = false;
   }
-  if (IsKeyDown(KEY_RIGHT))
+  else if (IsKeyDown(KEY_RIGHT))
   {
     playerPosition.x += 10;
+    face_right = true;
+    face_left = false;
+    face_top = false;
+    face_bot = false;
   }
-  if (IsKeyDown(KEY_LEFT))
+  else if (IsKeyDown(KEY_LEFT))
   {
     playerPosition.x -= 10;
+    face_left = true;
+    face_right = false;
+    face_top = false;
+    face_bot = false;
   }
 
   // Update the center tile
   centerTile = GetCenterTileLocation();
   // Update the camera
-  camera.target = (Vector2){.x = playerPosition.x - width / 2,
-                            .y = playerPosition.y - height / 2};
+  camera.target = (Vector2){.x = playerPosition.x - width / 2 + player.width / 20,
+                            .y = playerPosition.y - height / 2 + player.height / 40};
 };
 
 void DrawGameScreen(void)
@@ -77,8 +133,21 @@ void DrawGameScreen(void)
                   (centerTile.y * height - (height / 2)) + (height * j), WHITE);
     }
   }
-  DrawTextureEx(player, playerPosition, 0, 1, WHITE); // Draw the player
-  ClearBackground(RAYWHITE);
+
+  if (face_left)
+  {
+    // frameRec.width = -frameRec.width;
+    DrawTextureRec(player, (Rectangle){frameRec.x, frameRec.y, -frameRec.width, frameRec.height}, playerPosition, WHITE); // Draw part of the texture
+  }
+  else if (face_top || face_bot || face_right)
+  {
+    DrawTextureRec(player, frameRec, playerPosition, WHITE); // Draw part of the texture
+  }
+  else
+  {
+    DrawTextureRec(player, (Rectangle){0.0f, frameRec.y, frameRec.width, frameRec.height}, playerPosition, WHITE); // Draw part of the texture
+  }
+
   EndMode2D();
 };
 
