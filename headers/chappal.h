8bafc164d26ed646af67a765bb7e5e586e225a99
aahnik <daw@aahnik.dev>
2024-01-28 05:59:56 +0530

    implement multiple chappal types

--- headers/chappal.h
+++ headers/chappal.h
@@ -7,7 +7,24 @@
 #define ROTATION_SPEED_MIN -10.0f
 #define ROTATION_SPEED_MAX 10.0f
 
+#define MAX_CHAPPAL_TYPES 4
+
+// TODO: add resource links
+const static char *chappalSources[MAX_CHAPPAL_TYPES] =
+	{"resources/Square Buttons/Colored Square Buttons/Audio col_Square Button.png",
+	"resources/Square Buttons/Colored Square Buttons/Back col_Square Button.png",
+	"resources/Square Buttons/Colored Square Buttons/Down col_Square Button.png",
+	"resources/Square Buttons/Colored Square Buttons/X col_Square Button.png"};
+
+typedef enum {
+	KHANA=0,
+	DANDA,
+	JUTA,
+	CHAPPAL
+} ChappalType;
+
 typedef struct Chappal {
+	ChappalType type;
 	Texture2D texture;
 	Vector2 position;
 	Vector2 direction;
@@ -15,11 +32,11 @@ typedef struct Chappal {
 	float rotationSpeed;
 } Chappal;
 
-Chappal* CreateChappal(Texture2D texture, Vector2 target);
-void UpdateChappal(Chappal* chappal);
-void DrawChappal(Chappal* chappal);
+Chappal *CreateChappal(Texture2D textures[], Vector2 target);
+void UpdateChappal(Chappal *chappal);
+void DrawChappal(Chappal *chappal);
 void IncreaseSpeed();
 void SetSpeed(float newSpeed);
-void DestroyChappal(Chappal* chappal);
+void DestroyChappal(Chappal *chappal);
 
 #endif
