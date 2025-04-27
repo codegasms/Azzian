b2a44945afccfd5786bf95dd22e4863a9b0c56c2
virinci <revirinci@proton.me>
2025-04-28 00:01:34 +0530

    Use emscripten_set_main_loop instead of the while loop for web platform and handle audio device

--- src/main.c
+++ src/main.c
@@ -2,18 +2,26 @@
 #include <raylib.h>
 #include <raymath.h>
 
+#if defined(PLATFORM_WEB)
+#include <emscripten/emscripten.h>
+#endif
+
 GameScreen currentScreen = TITLE;
 
 void UpdateDrawFrame(void);
 
 static bool quitGame = false;
 
+const int screenWidth = 1280;
+const int screenHeight = 720;
+
 int main(void) {
 #ifdef NDEBUG
 	SetTraceLogLevel(LOG_NONE);
 #endif
 
-	InitWindow(1280, 720, "Azzian - DEBUG");
+	InitWindow(screenWidth, screenHeight, "Azzian - DEBUG");
+	InitAudioDevice();
 
 	// Change it later to Title screen. Player should not go directly to the game screen.
 	switch (currentScreen) {
@@ -30,14 +38,21 @@ int main(void) {
 
 	SetExitKey(KEY_NULL);
 
+#if defined(PLATFORM_WEB)
+	const int fps =
+		60; // Looks like you are rendering without using requestAnimationFrame for the main loop.
+	        // You should use 0 for the frame rate in emscripten_set_main_loop in order to use
+	        // requestAnimationFrame, as that can greatly improve your frame rates!
+	emscripten_set_main_loop(UpdateDrawFrame, fps, 1);
+#else
 	SetTargetFPS(60);
 	while (!WindowShouldClose()) {
-		ClearBackground(RAYWHITE);
 		UpdateDrawFrame();
 		if (quitGame) {
 			break;
 		}
 	}
+#endif
 
 	switch (currentScreen) {
 	case GAMEPLAY:
@@ -51,6 +66,7 @@ int main(void) {
 		break;
 	}
 
+	CloseAudioDevice();
 	CloseWindow();
 	return 0;
 }
@@ -82,6 +98,7 @@ void UpdateDrawFrame(void) {
 	}
 
 	BeginDrawing();
+	ClearBackground(RAYWHITE);
 
 	switch (currentScreen) {
 	case GAMEPLAY:
